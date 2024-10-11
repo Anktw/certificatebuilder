8f5a33769e6f3773dc810236d7659c8c1e333db6
Anktw <at792226@gmail.com>
2024-10-11 20:49:15 +0530

    removed get all certificate from frontend

--- src/app/verify/[id]/page.js
+++ src/app/verify/[id]/page.js
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 import VerifiedCertificatePreview from '@/app/components/VerifiedCertificatePreview';
 import dotenv from 'dotenv';
-
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DB;
